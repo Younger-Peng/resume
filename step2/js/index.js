f0e1160f205842000fa4aa2a1ca047bc1c4a39d5
plearner <953413537@qq.com>
2017-06-06 21:47:41 +0800

    添加新的作品链接

--- step2/js/index.js
+++ step2/js/index.js
@@ -1,3 +1,25 @@
 var $ = require('jquery');
-console.log($);
-console.log(1)
+
+//为导航栏添加点击事件，跳转到指定的板块
+(function(){
+  $('#about').on('click', function(){
+    $('body').animate({
+      scrollTop: 0
+    }, 500)
+  })
+  $('#work').on('click', function(){
+    $('body').animate({
+      scrollTop: 628
+    }, 500)
+  })
+  $('#skill').on('click', function(){
+    $('body').animate({
+      scrollTop: 1608
+    }, 500)
+  })
+  $('#contact').on('click', function(){
+    $('body').animate({
+      scrollTop: 2388
+    }, 500)
+  })
+})()
